58f64fb82daddfd87b2851b1209989c7eea2f53a
Under4u <89784115+Under4u@users.noreply.github.com>
2022-11-09 12:09:49 +0000

    Finish 4th lesson

--- src/test/java/qa/pixelparrot/SelTest.java
+++ src/test/java/qa/pixelparrot/SelTest.java
@@ -3,6 +3,7 @@ package qa.pixelparrot;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.*;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -12,17 +13,24 @@ public class SelTest {
     static void setUp(){
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
     }
 
     @Test
     void fillFormTest(){
+        String name = "Alex";
         open("/text-box");
 
-        $("[id=userName]").setValue("Alex");
+        $("[id=userName]").setValue(name);
         $("[id=userEmail]").setValue("alex@test.com");
         $("[id=currentAddress]").setValue("Street 1");
         $("[id=permanentAddress]").setValue("Street 2");
         $("[id=submit]").click();
+
+        $("[id=output]").shouldHave(text(name), text("Street 1"));
+
+        $("[id=output] [id=name]").shouldHave(text(name));
+        $("[id=output]").$("[id=name]").shouldHave(text(name));
 
 
 
